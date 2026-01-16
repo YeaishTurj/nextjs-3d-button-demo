00e58e3bb76e0cf2eb22f68ebcc7eb6d7985734a
YeaishTurj <yjturj12104@gmail.com>
2026-01-17 00:50:43 +0600

    update page.tsx

--- src/app/page_updated.tsx
+++ src/app/page_updated.tsx
@@ -12,10 +12,8 @@ export default function Home() {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      direction: "vertical",
       gestureOrientation: "vertical",
       smoothWheel: true,
-      smoothTouch: false,
     });
 
     function raf(time: number) {
